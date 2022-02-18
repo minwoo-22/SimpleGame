45077074f8752b6e544a72d0311399f41fd3d55a
start-27 <minw7092@naver.com>
2022-02-18 23:45:05 +0900

    Initial commit

---
+++ src/test/java/com/example/game/biginsight/BiginsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game.biginsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiginsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
