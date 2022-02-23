7bd1f6b3cb4769beefd8c24feec87782af6398b7
start-27 <minw7092@naver.com>
2022-02-23 17:07:19 +0900

    Chore : 몬스터 기본 스탯 향상

--- src/main/java/com/example/game/biginsight/attribute/MonsterAttribute.java
+++ src/main/java/com/example/game/biginsight/attribute/MonsterAttribute.java
@@ -1,8 +1,8 @@
 package com.example.game.biginsight.attribute;
 
 public interface MonsterAttribute {
-    int MAX_HP = 200;
-    int POWER = 25;
-    double ATTACK_SPEED = 0.4;
-    int DEFENSE = 5;
+    int MAX_HP = 400;
+    int POWER = 30;
+    double ATTACK_SPEED = 0.6;
+    int DEFENSE = 7;
 }
